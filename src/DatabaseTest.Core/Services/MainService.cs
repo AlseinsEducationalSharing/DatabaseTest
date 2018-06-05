d3045737a460f1348cd9ebfffe8a257e6c6bdbc4
Alsein <xyh951115@live.com>
2018-06-05 16:04:08 +1000

    rename

--- src/DatabaseTest.Core/Services/MainService.cs
+++ src/DatabaseTest.Core/Services/MainService.cs
@@ -11,27 +11,27 @@ namespace DatabaseTest.Services
 
         public void Main(string[] args)
         {
-            var testModel = DatabaseService["test"].GetRepository<Person>();
+            var person = DatabaseService["test"].GetRepository<Person>();
             var tt = DatabaseService.ToArray();
 
             var adam = new Person { Name = "Adam", Age = 10 };
             var eva = new Person { Name = "Eva", Age = 15 };
 
             //Add
-            testModel.Add(adam);
-            testModel.Add(eva);
+            person.Add(adam);
+            person.Add(eva);
 
             //Change
             adam.Age = 15;
-            testModel.Update(adam);
-            testModel.Update(x => x.Age == 15, new Person { Age = 20 });
+            person.Update(adam);
+            person.Update(x => x.Age == 15, new Person { Age = 20 });
 
             //Query
-            testModel.AsEnumerable().Select(x => $"Name={x.Name}, Age={x.Age}, Id={x.Id}").ForAll(Console.WriteLine);
+            person.AsEnumerable().Select(x => $"Name={x.Name}, Age={x.Age}, Id={x.Id}").ForAll(Console.WriteLine);
 
             //Delete
-            testModel.Remove(x => x.Name == "Adam");
-            testModel.Remove(eva);
+            person.Remove(x => x.Name == "Adam");
+            person.Remove(eva);
         }
     }
 }
